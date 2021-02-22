a4c13970df5e21d73023c4d992531d664db9ceae
haolaoshi <liujiahao.oak@gmail.com>
2021-02-22 00:08:34 -0500

    unix

---
+++ 01/03-unix.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <dirent.h>
+
+int main(int argc,char  **argv)
+{
+    DIR         *dp;
+    struct dirent   *dirp;
+
+
+    if (argc != 2){
+        printf("usage : ");
+        exit(1);
+    }
+
+    if ((dp = opendir(argv[1])) == NULL){
+
+        printf("Cannot %s",argv[1]);
+    }
+
+    while((dirp = readdir(dp)) != NULL)
+        printf("%s\n",dirp->d_name);
+    close(dp);
+    exit(0);
+}
